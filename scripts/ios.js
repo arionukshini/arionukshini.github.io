5d5e46714ab23f93af1ae6bee0c312e41675aebc
Diino <gameroni13@gmail.com>
2022-06-24 20:37:38 +0200

    Initial commit

---
+++ scripts/ios.js
@@ -0,0 +1,30 @@
+var alt = document.getElementById("alt");
+var sc = document.getElementById("sc");
+var iosgods = document.getElementById("iosgods");
+var jc = document.getElementById("jc");
+var esign = document.getElementById("esign");
+var dipa = document.getElementById("dipa");
+
+alt.onclick = function() {
+  window.open('https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fdiino-website.itsdiino.repl.co%2Ffiles%2Fdino_alt.json', '_blank');
+}
+
+sc.onclick = function() {
+  window.open('https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fdiino-website.itsdiino.repl.co%2Ffiles%2Fdino_alt.json', '_blank');
+}
+
+iosgods.onclick = function() {
+  window.open('https://iosgods.com/profile/6429469-itsdiino/', '_blank');
+}
+
+jc.onclick = function() {
+  window.open('https://jailedcreations.xyz/#menu', '_blank');
+}
+
+esign.onclick = function() {
+  window.open('https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fdiino-website.itsdiino.repl.co%2Ffiles%2Fdino_esign.json', '_blank');
+}
+
+esign.onclick = function() {
+  window.open('https://ipa.decrypt.day/categories', '_blank');
+}
---
+++ scripts/socials.js
@@ -0,0 +1,25 @@
+var yt = document.getElementById("yt");
+var dc = document.getElementById("dc");
+var gh = document.getElementById("gh");
+var rd = document.getElementById("rd");
+var tw = document.getElementById("tw");
+
+yt.onclick = function() {
+  window.open('https://www.youtube.com/ItsDino', '_blank');
+}
+
+dc.onclick = function() {
+  window.open('https://discord.gg/twXdceRPhq', '_blank');
+}
+
+gh.onclick = function() {
+  window.open('https://github.com/ItsDiino', '_blank')
+}
+
+rd.onclick = function() {
+  window.open('https://www.reddit.com/user/Biinosaur', '_blank')
+}
+
+tw.onclick = function() {
+  window.open('https://twitter.com/diinowastaken', '_blank')
+}

0316fcaf5c8cd4f4efc1bdaa9b22bc927841e562
Diino <gameroni13@gmail.com>
2024-04-22 17:49:24 +0200

    rebrand

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var icon = document.getElementById("icon");
+var initial1 = document.getElementById("initial1");
+var initial2 = document.getElementById("initial2");
 
-icon.onclick = function() {
-  document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")) {
-      icon.src = "../icons/sun.png";
+const rainbowColors = [
+    '#ffa600', // Orange
+    '#f8e538', // Yellow
+    '#43833d', // Green
+    '#3d83a9', // Blue
+    '#2f92fa', // Light Blue
+    '#b22eff', // Purple
+    '#d23b83', // Magenta
+    '#ff2e63', // Pink
+    '#f51616'  // Red
+];
+var currentColorIndex = 0
+
+function getRandomColor() {
+    if (currentColorIndex >= 8) {
+        currentColorIndex = 0
     } else {
-    icon.src = "../icons/moon.png";
-  }
+        currentColorIndex = currentColorIndex + 1;
+    }
+    return rainbowColors[currentColorIndex];
 }
+
+function handleMouseOver() {
+    var randomColor = getRandomColor();
+    document.documentElement.style.setProperty('--acc', randomColor);
+}
+
+initial1.addEventListener('mouseover', handleMouseOver);
+initial2.addEventListener('mouseover', handleMouseOver);
